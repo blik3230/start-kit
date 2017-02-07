b6173cf6b524fc19ff52d38c6247bf705f12f0eb
m.semenish <m.semenish@itua.com.ua>
2017-02-07 11:20:33 +0200

    added css glob in gulp

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -5,6 +5,7 @@ const sass = require('gulp-sass'),
 	sourcemaps = require('gulp-sourcemaps'),
 	rename = require('gulp-rename'),
 	autoprefixer = require('gulp-autoprefixer'),
+	cssGlobbing = require('gulp-css-globbing'),
 	watch = require('gulp-watch'),
 	config = {
 		src: ['./src/main.scss'],
@@ -17,9 +18,28 @@ module.exports = function (gulp, serv, defaultTask) {
 	
 	gulp.task('scss:build', function() {
 		
+		// return gulp.src(config.src)
+		// 	.pipe(sourcemaps.init())
+		// 	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+		// 	.pipe(autoprefixer())
+		// 	.pipe(sourcemaps.write())
+		// 	.pipe(rename('style.css'))
+		// 	.pipe(gulp.dest(config.dist))
+		// 	.pipe(serv.stream({match: '**/*.css'}));
+		
 		return gulp.src(config.src)
+			.pipe(cssGlobbing({
+				// Configure it to use SCSS files
+				extensions: ['.scss']
+			}))
 			.pipe(sourcemaps.init())
-			.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+			.pipe(sass({
+				cache_location: './cache',
+				cache: true,
+				outputStyle: 'expanded',
+				precision: 3,
+				includePaths: ['.']
+			}).on('error', sass.logError))
 			.pipe(autoprefixer())
 			.pipe(sourcemaps.write())
 			.pipe(rename('style.css'))
